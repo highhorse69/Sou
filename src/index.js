68211e46690c314b1003042f7c20309d559cba5b
Rohan Sahani <rohan.sahani.m97@gmail.com>
2021-05-13 05:02:45 +0530

    k

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {Provider} from 'react-redux';
+import { createStore,applyMiddleware,combineReducers} from 'redux';
+import { searchRobots,requestRobots } from './reducer';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk'
 import reportWebVitals from './reportWebVitals';
-
 import 'tachyons' ;
+import App from './container/App';
 
 
+const logger = createLogger();
+const rootReducer=combineReducers({ searchRobots,requestRobots })
 
+const store= 
+createStore(rootReducer,applyMiddleware(thunkMiddleware,logger))
 
 ReactDOM.render(
- <Apple />,
-  
+  <React.StrictMode>
+  <Provider store={store}>
+  <App  />
+  </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
